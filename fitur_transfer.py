57665f70f6afdd067a010d157e26652aec3c2d54
RUDI IRAWAN <50778467+rudiirawan26@users.noreply.github.com>
2021-01-17 19:35:43 +0700

    Fitur 1 & 2

--- fitur_transfer.py
+++ fitur_transfer.py
@@ -1,135 +1,46 @@
-user_id = 0
-loop = "n"
-user = [
-    {
-        "id": "1234",
-        "no_rekening": "1234567890",
-        "username": "test",
-        "pin": "4321",
-        "saldo": 0
-    },
-    {
-        "id": "4321",
-        "no_rekening": "0987654321",
-        "username": "test2",
-        "pin": "1234",
-        "saldo": 25000000
-    }
-]
-status_login = False
-pakai_atm = "y"
-
-
-def cek_login(p):
-    for us in user:
-        if us['pin'] == p:
-            return us
-    return False
-
-
-def cek_user(id):
-    for i in range(len(user)):
-        if user[i]['id'] == str(id):
-            return int(i)
-    return -1
-
-
-def cek_rekening(no):
-    for i in range(len(user)):
-        if str(user[i]['no_rekening']) == str(no):
-            return int(i)
-    return -1
-
-
-def tranfer_uang(uang, no_rekening):
-    index1 = cek_user(user_id)
-    index2 = cek_rekening(no_rekening)
-    if index1 >= 0:
-        if user[index1]['saldo'] >= int(uang):
-            user[index1]['saldo'] = user[index1]['saldo'] - int(uang)
-            user[index2]['saldo'] = user[index2]['saldo'] + int(uang)
-            print("Anda berhasil mentransfer uang Rp." + str(uang) + " ke Rekening " + no_rekening)
-            print("sisa saldo anda adalah Rp.", user[index1]['saldo'])
-        else:
-            print("Ops saldo anda tidak cukup")
-
-
-def ambil_uang(uang):
-    index1 = cek_user(user_id)
-    if index1 >= 0:
-        if user[index1]['saldo'] >= int(uang):
-            user[index1]['saldo'] = user[index1]['saldo'] - int(uang)
-            print("Anda berhasil menarik uang Rp." + str(uang))
-            print("sisa saldo anda adalah Rp.", user[index1]['saldo'])
-        else:
-            print("Ops saldo anda tidak cukup")
-
-
-while pakai_atm == "y":
-    while status_login == False:
-        print("SELAMAT DATANG DI ATM BANK BAGOOD")
-        print("Silahkan masukan pin anda")
-        pin = input("Masukan PIN : ")
-
-        cl = cek_login(pin)
-        if cl != False:
-            print("selamat data " + cl['username'])
-            user_id = cl['id']
-            status_login = True
-            loop = "y"
-        else:
-            print("")
-            print("Ops PIN anda salah")
-            print("")
-            print("")
-
-    while loop == "y" and status_login == True:
-        u = user[cek_user(user_id)]
-        print("SELAMAT DATANG DI ATM BANK BAGOOD")
-        print("1.Cek Saldo")
-        print("2.Transfer Uang")
-        print("3.Ambil Uang")
-        print("4.Logout")
-        print("5.Keluar ATM")
-        a = int(input("Silahkan pilih menu : "))
-        if a == 1:
-            print("")
-            print("Sisa Saldo anda adalah Rp.", u['saldo'])
-            print("")
-            print("")
-            loop = "n"
-        elif a == 2:
-            print("Untuk Mentransfer Uang Silahkan Masukan No Rekening Tujuan")
-            no_rek = input("Masukan No Rekening Tujuan : ")
-            cnk = cek_rekening(no_rek)
-
-            if cnk >= 0:
-                print("Nomor rekening ditemukan,silahkan masukan nominal yang yang akan di transfer")
-                nominal = input("Nominal Yang Akan Di Transfer : ")
-                tranfer_uang(nominal, no_rek)
-                print("")
-                loop = "n"
-            else:
-                print("")
-                print("Nomor Rekening Tujuan Tidak ditemukan atau tidak terdaftar")
-                print("")
-                loop = "n"
-
-        elif a == 3:
-            nominal = input("Nominal Yang Akan Di Tarik : ")
-            ambil_uang(nominal)
-            print("")
-            loop = "n"
-        elif a == 4:
-            status_login = False
-
-        elif a == 5:
-            status_login = False
-            loop = "n"
-            pakai_atm = "n"
-        else:
-            print("pilihan tidak tersedia")
-        if status_login == True:
-            input("kembali Ke menu (Enter) ")
-            print("")
-            loop = "y"
+data_a = open("transfer.txt")
+data_x = open("nasabah.txt")
+data_y = []
+data_z = []
+data_nama = []
+data_rekening = []
+data_saldo = []
+data_mentah = []
+data_transfer = []
+data_g = []
+for h in data_a:
+    data_mentah.append(h.strip())
+for i in data_mentah:
+    data_transfer.append(i.split(","))
+for g in range(len(data_transfer)):
+  if data_transfer[g][1] not in data_g:
+    data_g.append(data_transfer[g][1])
+for p in data_x:
+    data_y.append(p.strip())
+for c in data_y:
+    data_z.append(c.split(","))
+for a in range(len(data_z)):
+    data_rekening.append(data_z[a][0])
+    data_nama.append(data_z[a][1])
+    data_saldo.append(eval(data_z[a][2]))
+data_tunai = dict(zip(data_rekening, data_saldo))
+data_nasabah = dict(zip(data_rekening, data_nama))
+def menu():
+    print("Menu: ")
+    print("[1] Buka Rekening Bank\n[2] Setoran Tunai\n[3] Tarik Tunai\n[4] Transfer\n[5] Lihat Daftar Transfer\n[6] Keluar")
+print("** SELAMAT DATANG DI NF BANK **")
+menu()
+while True:
+    pilih = input("Masukkan menu pilihan anda: ")
+    if pilih == '1':
+        nama = input("Masukkan nama anda: ")
+        setoran = eval(input("Masukkan Setoran Awal: "))
+        import random, string
+        norek =  "REK" + ''.join(random.choice(string.digits) for _ in range(3))
+        print("Pembukaan dengan nomor rekening", norek, "atas nama", nama, "berhasil.\n")
+        data_nama.append(nama)
+        data_saldo.append(setoran)
+        data_rekening.append(norek)
+        data_tunai[norek] = setoran
+        data_nasabah[norek] = nama
+        menu()
